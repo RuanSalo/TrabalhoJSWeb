4b57775a48f9d65fc7e28204ebba966ef0f84fe3
RuanSalo <ruansalo03@gmail.com>
2021-04-17 19:03:18 -0300

    Pré jogo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const proxy = "https://cors-anywhere.herokuapp.com/";
+const proxy = "https://secret-ocean-49799.herokuapp.com/";
 let jogo;
 const elementos = {
 	cabecalho: document.querySelector("#cabecalho"),
@@ -10,12 +10,23 @@ const elementos = {
 		botao2: document.querySelector(".botao2"),
 		botao3: document.querySelector(".botao3"),
 		botao4: document.querySelector(".botao4"),
-		botaoCategoria: document.querySelector(".botaoCategoria"),
 		botaoConfirma: document.querySelector(".botaoConfirmar"),
 		botaoArmazenaPergunta: document.querySelector(".botaoArmazenaPergunta"),
 	},
-	listaCategorias: document.querySelector(".listaCategoriasDropdown")
+	selectCategoria: document.querySelector(".selectCategoria"),
 };
+
+const carregarCategorias = () => {
+	axios.get(`${proxy}https://opentdb.com/api_category.php`).then((response) => {
+		const categorias = response.data.trivia_categories;
+		for (const categoria of categorias) {
+			const option = document.createElement("option");
+			option.innerHTML += `<option value="${categoria.id}">${categoria.name}</option>`;
+			elementos.selectCategoria.appendChild(option);
+
+		}
+	});
+}
 
 const novoJogo = () => {
 	jogo = {
@@ -24,29 +35,18 @@ const novoJogo = () => {
 		pontos: undefined,
 	};
 
+	carregarCategorias();
 
 	elementos.cabecalho.textContent = "Bem Vindo!";
 	elementos.texto.textContent = "Selecione a dificuldade e categoria:";
 	elementos.botoes.botao1.textContent = "Fácil";
 	elementos.botoes.botao2.textContent = "Médio";
 	elementos.botoes.botao3.textContent = "Difícil";
-	elementos.botoes.botao4.classList.remove('escondido');
-	elementos.botoes.botao4.classList.add('escondido');
+	elementos.botoes.botao4.classList.remove("escondido");
+	elementos.botoes.botao4.classList.add("escondido");
 	elementos.botoes.botaoConfirma.textContent = "Confirmar";
-	elementos.botoes.botaoArmazenaPergunta.classList.remove('escondido');
-	elementos.botoes.botaoArmazenaPergunta.classList.add('escondido');
-	elementos.botoes.botaoCategoria.addEventListener('click', function () {
-		axios
-			.get(`${proxy}https://opentdb.com/api_category.php`)
-			.then((response) => {
-				const categorias = response.trivia_categories;
-				listaCategorias.innerHTML = '';
-				for (const categoria of categorias) {
-					listaCategorias.innerHTML = `<a id="${categoria.id}">${categoria.name}</a>`;
-				}
-			});
-	});
-	
+	elementos.botoes.botaoArmazenaPergunta.classList.remove("escondido");
+	elementos.botoes.botaoArmazenaPergunta.classList.add("escondido");
 };
 
 novoJogo();
